80ecbdf4a4d560b92cd19c03c17628363f6a67ed
Runyuan Yin <runyuan15@login1.hpcfund>
2025-12-02 13:03:32 -0600

    Dec 2 update: minimal-change integration + benchmark-aligned refactor

---
+++ runyuan_updates/runyuan_2025Dec2/iris_testing-main/baseline.py
@@ -0,0 +1,34 @@
+"""
+Baseline a2a implementation to compare perf against.
+"""
+
+import torch
+import torch.distributed as dist
+
+#def run(
+#    rank: int, tokens: torch.tensor, chunk_size: int, 
+#    batch: int, seq: int, hidden_dim: int, 
+#    num_experts: int, world_size: int, 
+#    general_a2a: bool, shmem
+#):
+def run(
+    rank: int,
+    tokens: torch.Tensor,
+    expert_weights: torch.Tensor,
+    chunk_size: int,
+    batch: int,
+    seq: int,
+    hidden_dim: int,
+    num_experts: int,
+    world_size: int,
+    general_a2a: bool,
+    shmem,
+):
+    if general_a2a:
+        pass ## Not implemented yet. ##
+    else:
+        tokens_recv = torch.zeros(chunk_size * world_size, hidden_dim, dtype=tokens.dtype).to("cuda" if torch.cuda.is_available() else "cpu")
+        dist.all_to_all_single(tokens_recv, tokens)
+
+        torch.cuda.synchronize()
+        #print(f'[rank: {rank}], summed token buffer: {tokens_recv.sum()}')
